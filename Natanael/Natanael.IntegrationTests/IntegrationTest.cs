64e18a57513fd7538dfb150a555b8a4eb49f75bc
Natanael <natanaelsantosbr@gmail.com>
2021-02-02 01:52:45 -0300

    Criado Teste Unitario com EF em Memoria e testado a controller Post

---
+++ Natanael/Natanael.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Natanael.Web;
+using Natanael.Web.Contracts.V1;
+using Natanael.Web.Contracts.V1.Requests;
+using Natanael.Web.Contracts.V1.Responses;
+using Natanael.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Natanael.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient _client;
+        public IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+
+            _client = appFactory.CreateClient();
+
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response = await this._client.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+            return await response.Content.ReadAsAsync<PostResponse>();
+
+
+        }
+
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await this._client.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "natanaelsantosbr@gmail.com",
+                Password = "Natanael@1030!"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+
+            return registrationResponse.Token;
+
+            
+        }
+    }
+}
---
+++ Natanael/Natanael.IntegrationTests/PostsControllersTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Natanael.Web.Contracts.V1;
+using Natanael.Web.Domain;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Natanael.IntegrationTests
+{
+    public class PostsControllersTests : IntegrationTest
+    {
+        public PostsControllersTests()
+        {
+
+        }
+        [Fact]
+        public async Task GetAll_WithoutEmptyAnyPosts_ReturnEmptyResponse()
+        {
+            //Arrange
+            await this.AuthenticateAsync();
+
+            //Act
+            var response = await this._client.GetAsync(ApiRoutes.Posts.GetAll);
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPosts_WhenPostExistsInTheDatabase()
+        {
+            //Arrange
+            await this.AuthenticateAsync();
+            var createdPost = await this.CreatePostAsync(new Web.Contracts.V1.Requests.CreatePostRequest { Name = "Post Tests" });
+
+            //Act
+            var response = await this._client.GetAsync(ApiRoutes.Posts.Get.Replace("{postId}", createdPost.Id.ToString()));
+
+            //Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnPost = await response.Content.ReadAsAsync<Post>();
+            returnPost.Id.Should().Be(createdPost.Id);
+            returnPost.Name.Should().Be("Post Tests");
+            
+        }
+
+    }
+}
