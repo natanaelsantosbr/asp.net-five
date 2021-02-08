39eb40e71c089c320a810c337e1bb86967ecbe5e
Natanael <natanaelsantosbr@gmail.com>
2021-02-08 15:27:08 -0300

    Autenticação baseada em ApiKey

---
+++ Natanael/Natanael.Web/Controllers/V1/SecretController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Natanael.Web.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Natanael.Web.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : Controller
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I have no secrets");
+        }
+
+
+    }
+}
---
+++ Natanael/Natanael.Web/Filters/ApiKeyAuthAttribute.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Natanael.Web.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    public class ApiKeyAuthAttribute : Attribute, IAsyncActionFilter
+    {
+        private const string ApiKeyHeaderName = "Apikey";
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if(!context.HttpContext.Request.Headers.TryGetValue(ApiKeyHeaderName, out var potentitalApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+            var configuration = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            var apiKey = configuration.GetValue<string>(ApiKeyHeaderName);
+
+            if(!apiKey.Equals(potentitalApiKey))
+            {
+                context.Result = new UnauthorizedResult();
+                return;
+            }
+
+
+            await next();
+
+        }
+    }
+}
